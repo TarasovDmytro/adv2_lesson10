b27535c346661f890692e01ff1a1ae59ce3b1a58
TarasovDmytro <tarasov.personal@gmail.com>
2021-05-05 19:02:09 +0300

    Обязательная программа

---
+++ src/ua/tarasov/hw3/Task2.java
@@ -0,0 +1,28 @@
+package ua.tarasov.hw3;
+//        Удалить из строки пробелы и определить, является ли она перевертышем
+//        Ввести строковую переменную. Удалить из нее все пробелы.
+//        После этого определить, является ли она палиндромом (перевертышем),
+//        т.е. одинаково пишется как с начала, так и с конца.
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("Введите строку для проверки - является ли она палиндромом:");
+        Scanner scan = new Scanner(System.in);
+        String strInput = scan.nextLine();
+        change(strInput);
+    }
+
+    private static void change(String strInput) {
+
+        String strChange = strInput.replaceAll("\\s", "");
+        StringBuilder stReverse = new StringBuilder(strChange).reverse();
+        if (strChange.equalsIgnoreCase(String.valueOf(stReverse))) {
+            System.out.println("Введенная строка является палиндромом");
+        } else {
+            System.out.println("Введенная строка не является палиндромом");
+        }
+
+    }
+
+}
---
+++ src/ua/tarasov/hw3/Task3.java
@@ -0,0 +1,25 @@
+package ua.tarasov.hw3;
+//Количество слов в строке
+//Вводится строка, состоящая из слов, разделенных пробелами.
+//Требуется посчитать количество слов в ней.
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Введите строку для проверки на количество в ней слов:");
+        Scanner scan = new Scanner(System.in);
+        String strInput = scan.nextLine();
+        System.out.println("В строке " + numSpases(strInput) + " слов");
+    }
+
+    private static int numSpases(String strInput) {
+        int word = 1;
+        for (int i = 0; i < strInput.length(); i++) {
+            word += (Character.isWhitespace(strInput.charAt(i))) ? 1 : 0;
+        }
+        return word;
+
+
+    }
+}
