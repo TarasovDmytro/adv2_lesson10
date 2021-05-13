98ac1244ec2304ebd450001e2c11c4d3da71afc3
TarasovDmytro <tarasov.personal@gmail.com>
2021-05-13 14:07:53 +0300

    HW4 Task1 исправлена формула

--- src/ua/tarasov/hw4/Task1.java
+++ src/ua/tarasov/hw4/Task1.java
@@ -5,8 +5,8 @@ import java.util.Random;
 
 public class Task1 {
     public static void main(String[] args) {
-        int[] intArray = new int[400];
-        fillRandArray(intArray);
+        int[] intArray = new int[]{1,2,3,4,5};
+//        fillRandArray(intArray);
         System.out.println(Arrays.toString(intArray));
         System.out.println("Average = " + averAge(intArray));
         System.out.println("Geometric mean = " + geoMean(intArray));
@@ -15,13 +15,16 @@ public class Task1 {
     private static void fillRandArray(int[] intArray) {
         Random rand = new Random();
         for (int i = 0; i < intArray.length; i++) {
-            intArray[i] = rand.nextInt(10000);
+            intArray[i] = rand.nextInt(10);
         }
     }
 
     private static double geoMean(int[] intArray) {
-        double sum = Arrays.stream(intArray).asDoubleStream().sum();
-        return Math.pow(sum, (1.0 / intArray.length));
+        double productElem = intArray[0];
+        for (int i = 1; i < intArray.length; i++) {
+            productElem *= intArray[i];
+        }
+        return Math.pow(productElem, (1.0 / intArray.length));
     }
 
     private static double averAge(int[] intArray) {
