8292108c23e9107145efa2de8a57def4d17afb7b
TarasovDmytro <tarasov.personal@gmail.com>
2021-05-13 13:53:12 +0300

    HW5 Task2 - одномерный массив

--- src/ua/tarasov/hw5/Task2.java
+++ src/ua/tarasov/hw5/Task2.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        int[][] intArray = new int[3][3];
+        int[] intArray = new int[10];
         fillArray(intArray);
-        for (int[] innerArray : intArray) {
-            System.out.println(Arrays.toString(innerArray));
-        }
+        System.out.println(Arrays.toString(intArray));
         if (orderingCheck(intArray)) {
             System.out.println("Массив упорядочен по невозрастанию");
         } else {
@@ -17,31 +15,27 @@ public class Task2 {
         }
     }
 
-    private static boolean orderingCheck(int[][] intArray) {
+    private static boolean orderingCheck(int[] intArray) {
         boolean check = false;
-        int temp = intArray[0][0];
-        for (int[] ints : intArray) {
-            for (int anInt : ints) {
-                if (anInt <= temp) {
-                    check = true;
-                    temp = anInt;
-                } else {
-                    check = false;
-                    break;
-                }
+        int temp = intArray[0];
+        for (int i : intArray) {
+            if (i <= temp) {
+                check = true;
+                temp = i;
+            } else {
+                check = false;
+                break;
             }
         }
         return check;
     }
 
-    private static int[][] fillArray(int[][] intArray) {
-        System.out.println("Задайте значения массива целых чисел [ " + intArray[0].length + " ][ " + intArray.length + " ]");
+    private static int[] fillArray(int[] intArray) {
+        System.out.println("Задайте значения массива целых чисел [ " + intArray.length + " ]");
         Scanner scan = new Scanner(System.in);
         for (int i = 0; i < intArray.length; i++) {
-            for (int j = 0; j < intArray[i].length; j++) {
-                System.out.println("  Введите целое число как элемент массива с индексом [ " + i + ";" + j + " ]:");
-                intArray[i][j] = scan.nextInt();
-            }
+            System.out.println("  Введите целое число как элемент массива с индексом [ " + i + " ]:");
+            intArray[i] = scan.nextInt();
         }
         return intArray;
     }
