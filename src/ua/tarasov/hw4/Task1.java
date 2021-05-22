2f99a9a77c97268b6fe81a241a0a066819fb4447
TarasovDmytro <tarasov.personal@gmail.com>
2021-05-22 16:51:42 +0300

    modul1

--- src/ua/tarasov/hw4/Task1.java
+++ src/ua/tarasov/hw4/Task1.java
@@ -6,7 +6,6 @@ import java.util.Random;
 public class Task1 {
     public static void main(String[] args) {
         int[] intArray = new int[]{1,2,3,4,5};
-//        fillRandArray(intArray);
         System.out.println(Arrays.toString(intArray));
         System.out.println("Average = " + averAge(intArray));
         System.out.println("Geometric mean = " + geoMean(intArray));
--- src/ua/tarasov/modul1/Task2.java
+++ src/ua/tarasov/modul1/Task2.java
@@ -3,21 +3,79 @@ package ua.tarasov.modul1;
 // и клетка в которую пробуют его передвинуть за 1 ход. Программа должна проверить,
 // возможно ли это сделать. *
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        String[][] array = new String[8][8];
-        for (int i = 0; i < array.length; i++) {
-            array[i][1] = ("a" + i);
-            array[i][1] = ("b" + i);
-            array[i][1] = ("c" + i);
-            array[i][1] = ("d" + i);
-            array[i][1] = ("e" + i);
-            array[i][1] = ("f" + i);
-            array[i][1] = ("g" + i);
-            array[i][1] = ("h" + i);
-        }
-        System.out.println(Arrays.toString(array));
+        String[][] array = chessBord();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите начальное расположение фигуры Конь на шахматной доске (в формате: а1):");
+        String firstLocation = scan.next();
+        System.out.println("Введите предполагаемое расположение фигуры Конь на шахматной доске (в формате: а1):");
+        String nextLocation = scan.next();
+        checkTurn(array, firstLocation, nextLocation);
     }
+
+    private static String[][] chessBord() {
+        String[][] chessBord = new String[8][8];
+        String str = null;
+        for (int i = 0; i < chessBord.length; i++) {
+            for (int j = 0; j < chessBord[i].length; j++) {
+                if (i == 0) str = "h";
+                if (i == 1) str = "g";
+                if (i == 2) str = "f";
+                if (i == 3) str = "e";
+                if (i == 4) str = "d";
+                if (i == 5) str = "c";
+                if (i == 6) str = "b";
+                if (i == 7) str = "a";
+                chessBord[i][j] = (str + (j + 1));
+            }
+        }
+        return chessBord;
+    }
+
+    private static void checkTurn(String[][] array, String startLocation, String finishLocation) {
+        int sLocX = getCoordinateX(array, startLocation);
+        int sLocY = getCoordinateY(array, startLocation);
+        int fLocX = getCoordinateX(array, finishLocation);
+        int fLocY = getCoordinateY(array, finishLocation);
+        if (Math.abs(fLocX - sLocX) == 2 && Math.abs(fLocY - sLocY) == 1 || Math.abs(fLocX - sLocX) == 1 && Math.abs(fLocY - sLocY) == 2) {
+            System.out.println("Да, такой ход возможен!");
+        } else {
+            System.out.println("Нет, такой ход не возможен!");
+        }
+    }
+
+    private static int getCoordinateX(String[][] array, String str) {
+        int x = -1;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (str.equals(array[i][j])) {
+                    x = i;
+                    break;
+                }
+            }
+        }
+        if (x == -1) {
+            System.out.println("Это за пределами доски!");
+        }
+        return x;
+    }
+
+    private static int getCoordinateY(String[][] array, String str) {
+        int y = 0;
+        for (String[] strings : array) {
+            for (int j = 0; j < array.length; j++) {
+                if (str.equals(strings[j])) {
+                    y = j;
+                    break;
+                }
+            }
+        }
+        return y;
+    }
+
 }
+
+
