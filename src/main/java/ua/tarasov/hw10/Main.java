edfe3f0858c5016bfb386c0f5ad49c8003d4a1de
TarasovDmytro <tarasov.personal@gmail.com>
2021-06-01 10:26:43 +0300

    new branch

---
+++ src/main/java/ua/tarasov/hw10/Main.java
@@ -0,0 +1,11 @@
+package ua.tarasov.hw10;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Report report = Report.builder().header("Header").body("Body").build();
+        Report report1 = report.toBuilder().footer("Footer").build();
+
+        System.out.printf("");
+    }
+}
